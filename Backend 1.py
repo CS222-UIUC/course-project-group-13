1d7e7898460a2d6c7d823fe9062b6e45858e5dc0
Rohith Sanjay <92739890+rohiths2@users.noreply.github.com>
2022-09-13 17:02:12 -0500

    Placeholders with a few lines of code to test Git upload
    Placeholders with a few lines of code to test Git upload

---
+++ Backend 1.py
@@ -0,0 +1,13 @@
+
+#I wrote this code just as placeholder of what we might build up on later
+password1 = "1234"
+password2 = "5678"
+level = 0
+
+def checkPassword(input):
+    if input == password1:
+        print("Correct")
+        level++
+    else:
+        print("Incorrect")
+
