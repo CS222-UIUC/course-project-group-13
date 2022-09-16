6de3cfdf0be19c2dc316e076b72cf0c9fd3b53d5
Rohith Sanjay <92739890+rohiths2@users.noreply.github.com>
2022-09-16 14:40:07 -0500

    Additional functions (point modifier and inventory)
    I added additional functionality to the password-checking function, which displays the amount of points gained, allows for multiple attempts (and thus less points each attempt), and presents a different message once all attempts are up. I added a second function like this for a second puzzle. I also added a function representing when a user would obtain a key or prize that can either be saved to the inventory or be used to unlock the next level

--- Backend 1.py
+++ Backend 1.py
@@ -1,13 +1,44 @@
 
-#I wrote this code just as placeholder of what we might build up on later
-password1 = "1234"
-password2 = "5678"
-level = 0
+#Ongoing backend development of puzzles to solve
+password1 = "segfault"
+password2 = "runtime"
+level = 1 #indicates the current number of the game's level
+points = 0 #the number of points the player has
+increase = 3 #how many more points can be gained when a question is answered correctly
+hasKeyOne = false #checks if a player has obtained a key or prize from the inventory
 
-def checkPassword(input):
+def clueOnePassword(input): #check if player solves clue one correctly
     if input == password1:
-        print("Correct")
-        level++
+        print("Correct! You earned " + increase + " points.")
+        points = points + increase
     else:
-        print("Incorrect")
+        increase = increase - 1
+        if (increase > 0):
+            print("Incorrect. You have " + increase + " tries remaining.")
+        else:
+            print("This puzzle is now unavailable")
+        
+        
+ def clueTwoPassword(input): #check if player solves clue two correctly
+    if input == password2:
+        print("Correct! You earned " + increase + " points.")
+        points = points + increase
+    else:
+        increase = increase - 1
+        if (increase > 0):
+            print("Incorrect. You have " + increase + " tries remaining.")
+        else:
+            print("This puzzle is now unavailable")
+        
+  def getKeyOneForInventory(): #player adds a new key to the inventory that allows for more features or a higher level
+    required = 3
+    if points < required:
+        print("Not enough points available. Continue solving puzzles")
+    else:
+        print("Description: this key unlocks the next level.") 
+        ++level
+        points = points - required
+        
+        
+
 
