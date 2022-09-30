3e8f41dac9056edc7be75015d274c013d88db883
Sydney Liu <syliu6@illinois.edu>
2022-09-30 18:22:55 -0500

    created barebones login & registration pages for user, started setting up user profile

---
+++ models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    points = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.username
