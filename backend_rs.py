8436191768b0f28de9330b81c9c3fb842b4aef5e
Rohith Sanjay <92739890+rohiths2@users.noreply.github.com>
2022-10-09 23:41:21 -0500

    Added maze and matching puzzles' functions, and added backup login functions

--- backend_rs.py
+++ backend_rs.py
@@ -6,6 +6,7 @@ PASSWORD1 = "vector"
 PASSWORD2 = "This project deserves an A!"
 PASSWORD3 = "binary"
 YEAR = 1998
+DIRECTIONS = "DRDLDLDRD"
 
 level = 1  # indicates the current number of the game's level
 points = 0  # the number of points the player has
@@ -14,6 +15,28 @@ keyone = 0  # checks if a player has obtained a key or prize from the inventory
 keytwo = 0
 keythree = 0
 completed = 0
+matchlist = (['Z', 'Z', 'Z', 'Z', 'Z'])
+index = 0
+credentials = ([["testusername", "testpassword", "testscore"]])
+
+
+def create_account_backup(username, password):
+    """backup function for creating new profile"""
+    global credentials
+    credentials.append([username, password, str(0)])
+    
+    
+def sign_in_backup(username, password):
+    """backup function for signing in"""
+    for i in range(len(credentials)):
+        if credentials[i][0] == username:
+            if credentials[i][1] == password:
+                print("Sign in successful")
+                print("Your score is " + credentials[i][2])
+                return 1
+    print("Incorrect username or password")
+    return 0
+    
 
 def level_changer():
     """allows player to continue to the next stage based on items in inventory"""
@@ -26,7 +49,7 @@ def level_changer():
     global increase
     level = level + 1
     points = 0
-    increase = 3
+    increase = 5
     if level == 3:
         if keythree == 1:
             print("Congratulations! You are able to move on to the next room.")
@@ -126,11 +149,82 @@ def get_key_one_for_inventory():
         keyone = 1
         points = points - required
         level_changer()
+        
+        
+# this puzzle below is used to solve a maze. Player types in directions of the maze
+def directional_lock(directions_input):
+    """checks if player correctly has solved the maze"""
+    global points
+    global increase
+    global DIRECTIONS
+    print("9 character directional code: up=U, down=D, left=L, right=R")
+    if directions_input == DIRECTIONS:
+        points = points + increase
+        print("Correct! You earned " + str(increase) + " points for a total of " + str(points))
+        return 1 #to indicate to the puzzle that it is solved
+    else:
+        increase = increase - 1
+        if increase > 0:
+            print("Incorrect. You have " + str(increase) + " tries remaining.")
+            return 0 #to indicate to the puzzle that it is unsolved
+        else:
+            print("This puzzle is now unavailable")
+            return -1 #to indicate that the puzzle is unavailable
+
+
+def matching_display():
+    print("Match the following terms on the right side with the clues on the left side:")
+    print("1. Rotation                                   A. DFS")
+    print("2. Stack                                      B. * or &")
+    print("3. Segmentation-fault-causer               C. Valgrind")
+    print("4. Allows for change                         D. AVL")
+    print("5. Memory                                   E. NULL")
+
+
+def create_match(letter):
+    global matchlist
+    global index
+    if (index < 5):
+        matchlist[index] = letter
+        index = index + 1
+
+def reset_matchlist():
+    global matchlist
+    global index
+    index = 0
+    create_match('Z')
+    create_match('Z')
+    create_match('Z')
+    create_match('Z')
+    create_match('Z')
+    index = 0
+
+def check_match():
+    global matchlist
+    global points
+    global increase
+    wrong = 0
+    correctmatch = (['D', 'A', 'E', 'B', 'C'])
+    for i in range(len(matchlist)):
+        if matchlist[i] != correctmatch[i]:
+            print("The " + str(i+1) +" entry is wrong")
+            reset_matchlist()
+            wrong = wrong + 1
+            return 0
+    if (wrong == 0):
+        print("Correct! You earned " + str(increase) + " points for a total of " + str(points))
+        points = points + increase
+    return 1
 
         
 def print_points() -> int:
     """getter for points"""
     return points
+
+
+def print_matchlist() -> list[str]:
+    """getter for matchlist"""
+    return matchlist
 
 
 def print_increase() -> int:
